98a67f68df87c2ce2dbcea6e11ec0bb6b68bb52d
Tafhim Ul Islam <tafhim.ul.islam@gmail.com>
2013-03-10 11:51:14 +0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sphinx::Application.config.secret_token = 'd6a4da89b88cce71a47b06e94bca23a07eefc1c6041af861e3425a3da6e1758f32e6209009ad153c16cd5a175b5b5b5a2cadca3eb0852817a67496a61bcbcf1c'
